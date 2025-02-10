cd38c76d933b7ffa4c3644b822ea23bb92cdd793
ABNER RIVERA <abner500edrey@gmail.com>
2025-02-10 16:30:16 -0500

    fix: Create acc no visible in mobile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
 				) : (
 					<button
 						onClick={() => navigate('/login')}
-						className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer"
+						className="bg-primary text-white px-8 py-3 rounded-full font-light cursor-pointer"
 					>
 						Create account
 					</button>
